869a52d2db420e0c15afa4b97a91eddc5064fdfb
devrocha03 <devrocha03@gmail.com>
2025-06-30 21:38:49 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Guegue, se você me ama, vai na cozinha e busca um doce pra mim!</h1>
+    return <h1>Guegue, se você me ama, vai na cozinha e busca um doce pra mim! :D </h1>
     
 }
 
