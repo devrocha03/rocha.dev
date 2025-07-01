41db9be31174b3b5a18ddc10f228a094d7ec47ed
devrocha03 <devrocha03@gmail.com>
2025-07-01 17:16:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,4 @@ function Home() {
     <h1>Vou tirar você da Progen, em nome de Jesus! Guegue Amo muito você!</h1>
   );
 }
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
