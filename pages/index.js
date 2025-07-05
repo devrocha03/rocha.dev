b0ecb14779a8c36d16d98ed87179d225d6891aec
devrocha03 <devrocha03@gmail.com>
2025-07-05 15:21:58 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-function Home() {
-  return (
-    <h1>Vou tirar você da Progen, em nome de Jesus! Guegue Amo muito você!</h1>
-  );
-}
+<html>
+<head>
+  <title>Exemplo JavaScript</title>
+</head>
+<body>
 
-export default Home;
+  <h1>Olá, visitante!</h1>
+
+  <button onclick="mostrarMensagem()">Clique aqui</button>
+
+  <script>
+    function mostrarMensagem() {
+      alert("Você clicou no botão! 🎉");
+    }
+  </script>
+
+</body>
+</html>
+
