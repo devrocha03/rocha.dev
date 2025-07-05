2e1fdf536c55eff1fa21d8bf0156a23f0d747f06
devrocha03 <devrocha03@gmail.com>
2025-07-05 16:49:34 +0000

    adiciona frase

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
 function Home() {
   return (
     <h1>
-      Vou tirar você da Progen, em nome de Jesus! Guegue Amo muito vocêeeee! 🙌
+      Vou tirar você da Progen, em nome de Jesus! Guegue Amo muito você! 🙌
     </h1>
-    
-   <button onclick="mostrarMensagem()">Clique aqui</button>
-
-  <script>
-    function mostrarMensagem()
-      alert("Você clicou no botão! 🎉");
-    
-  </script>
-
-</body>
-</html>
   );
 }
 export default Home;
